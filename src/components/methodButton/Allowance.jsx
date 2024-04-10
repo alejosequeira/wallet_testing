1145057a3f81f8fa9956337ceaaf83af099d0479
Sebastian Camia <logicalbrainstudio@gmail.com>
2024-04-10 18:00:11 -0300

    Approve params (#18)
    * Add manual params to Watch Asset
    
    * Add tokens to Watch Assets
    
    * Add params to ERC20Approve

--- src/components/methodButton/Allowance.jsx
+++ src/components/methodButton/Allowance.jsx
@@ -1,10 +1,10 @@
-"use client"
-import React, { useState, useEffect } from 'react';
-import Web3 from 'web3';
-import contractAbi from '../.././api/abiContractFunction.json';
-import erc20ABI from '../.././api/abiContractErc20.json';
-import { handleGetEthAccounts } from '@/utils/web3';
-import AlertComponent from '@/components/mainLayout/Alert';
+"use client";
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import contractAbi from "../.././api/abiContractFunction.json";
+import erc20ABI from "../.././api/abiContractErc20.json";
+import { handleGetEthAccounts } from "@/utils/web3";
+import AlertComponent from "@/components/mainLayout/Alert";
 
 const PermitAllowance = ({ contract }) => {
     const [erc20Approve, setERC20Approve] = useState("");
@@ -12,34 +12,37 @@ const PermitAllowance = ({ contract }) => {
     const [erc20Check, setERC20Check] = useState("");
     const [erc721Allow, setERC721Allow] = useState("");
     const [tokenContractAddress, setTokenContractAddress] = useState(contract);
-    const [owner, setOwner] = useState('');
-    const [spender, setSpender] = useState('0x3b539558C6465968ccfDe3A731bF63d6d4D8B85D');
+    const [owner, setOwner] = useState("");
+    const [spender, setSpender] = useState("0x3b539558C6465968ccfDe3A731bF63d6d4D8B85D");
     const [value1, setValue] = useState("1000000000000000000");
     const [deadline, setDeadline] = useState(9999999999);
-    const [signature, setSignature] = useState('0xfcf6af9335fa6b0a63ef0f2128fb923a810e1f575cf6565fe0e474352763e1287eb0750cd9755b59ed9ad301b6489d591736c071494ad2cef9629a6fd41f0dcf1b');
-    const [r1, setR] = useState('');
-    const [s1, setS] = useState('');
-    const [v1, setV] = useState('');
+    const [signature, setSignature] = useState(
+        "0xfcf6af9335fa6b0a63ef0f2128fb923a810e1f575cf6565fe0e474352763e1287eb0750cd9755b59ed9ad301b6489d591736c071494ad2cef9629a6fd41f0dcf1b"
+    );
+    const [r1, setR] = useState("");
+    const [s1, setS] = useState("");
+    const [v1, setV] = useState("");
     const [showForm, setShowForm] = useState(false);
     const [showFormAllowance, setShowFormAllowance] = useState(false);
     const [showForm721, setShowForm721] = useState(false);
     const [toggleHashApprove, setToggleHashApprove] = useState(false);
+    const [toggleApprove, setToggleApprove] = useState(false);
     const [toggleHash, setToggleHash] = useState(false);
     const [toggleCheck, setToggleCheck] = useState();
     const [toggleHash721, setToggleHash721] = useState(false);
-    const [erc721TokenAddress, setErc721TokenAddress] = useState('0x54Ad10aAf97875385e3415314a43AA4c87597Fa0');
-    const [operator, setOperator] = useState('0x3b539558c6465968ccfde3a731bf63d6d4d8b85d');
-    const [contractAddress, setContractAddress] = useState('0xE4A3464499562127C3049517066B5Cb409521906');
+    const [erc721TokenAddress, setErc721TokenAddress] = useState("0x54Ad10aAf97875385e3415314a43AA4c87597Fa0");
+    const [operator, setOperator] = useState("0x3b539558c6465968ccfde3a731bf63d6d4d8b85d");
+    const [contractAddress, setContractAddress] = useState("0xE4A3464499562127C3049517066B5Cb409521906");
     const contractABI = contractAbi;
     const erc20Abi = erc20ABI;
-    const [isCopied, setIsCopied] = useState(false)
+    const [isCopied, setIsCopied] = useState(false);
 
     useEffect(() => {
         const extractSignatureParts = async (signature) => {
-            setR("0x" + signature.slice(2, 66))
-            setS("0x" + signature.slice(66, 130))
-            setV(parseInt(signature.slice(130, 132), 16))
-        }
+            setR("0x" + signature.slice(2, 66));
+            setS("0x" + signature.slice(66, 130));
+            setV(parseInt(signature.slice(130, 132), 16));
+        };
         handleGetEthAccounts(setOwner);
         extractSignatureParts(signature);
     }, []);
@@ -51,14 +54,13 @@ const PermitAllowance = ({ contract }) => {
         try {
             const web3 = new Web3(window.ethereum);
             const tokenContract = new web3.eth.Contract(erc20Abi, tokenContractAddress);
-    
+
             // const value = web3.utils.toBN(value1);
             const value = value1;
-    
+
             const estimatedGas = await tokenContract.methods.approve(spender, value).estimateGas({ from: owner });
-    
             const tx = await tokenContract.methods.approve(spender, value).send({ from: owner, gas: estimatedGas });
-    
+
             setERC20Approve(tx);
             setToggleHashApprove(true);
             console.log("Approval succeeded", tx);
@@ -73,28 +75,28 @@ const PermitAllowance = ({ contract }) => {
             const web3 = new Web3(window.ethereum);
             const tokenContract = new web3.eth.Contract(erc20Abi, tokenContractAddress);
             // const value = web3.utils.toBN(value1);
-            const value= value1;
-    
+            const value = value1;
+
             const r = "0x" + signature.slice(2, 66);
             const s = "0x" + signature.slice(66, 130);
             const v = parseInt(signature.slice(130, 132), 16);
-            
+
             setR(r);
             setS(s);
             setV(v);
-    
+
             if (!web3.utils.isHexStrict(r) || !web3.utils.isHexStrict(s) || isNaN(v)) {
                 console.error("Invalid signature format.");
                 return;
             }
-    
+
             const estimatedGas = await tokenContract.methods.permit(owner, spender, value, deadline, v, r, s).estimateGas({ from: owner });
-    
+
             const tx = await tokenContract.methods.permit(owner, spender, value, deadline, v, r, s).send({ from: owner, gas: estimatedGas });
-    
+
             setERC20Allow(tx);
             setToggleHash(true);
-    
+
             console.log("Transaction details:", {
                 owner: owner,
                 spender: spender,
@@ -104,7 +106,7 @@ const PermitAllowance = ({ contract }) => {
                 r: r,
                 s: s,
                 estimatedGas: estimatedGas,
-                txHash: tx.transactionHash
+                txHash: tx.transactionHash,
             });
             console.log("Permit succeeded", tx);
         } catch (error) {
@@ -112,20 +114,19 @@ const PermitAllowance = ({ contract }) => {
             setToggleHash(false);
             console.error("Permit failed:", error);
         }
-    };   
-    
+    };
 
     const checkERC20Allowance = async () => {
         try {
             const web3 = new Web3(window.ethereum);
             const tokenContract = new web3.eth.Contract(erc20Abi, tokenContractAddress);
-    
+
             const currentAllowance = await tokenContract.methods.allowance(owner, spender).call();
             console.log(`Current allowance for spender is: ${currentAllowance}`);
-    
-            const readableAllowance = web3.utils.fromWei(currentAllowance, 'ether');
+
+            const readableAllowance = web3.utils.fromWei(currentAllowance, "ether");
             console.log(`Readable allowance for spender is: ${readableAllowance} tokens`);
-    
+
             setERC20Check(`Allowance for spender is: ${readableAllowance} tokens`);
             setToggleCheck(true);
         } catch (error) {
@@ -133,7 +134,7 @@ const PermitAllowance = ({ contract }) => {
             setERC20Check("Failed to check allowance. See console for more details.");
             setToggleCheck(false);
         }
-    };    
+    };
 
     async function setERC721Allowance() {
         try {
@@ -141,13 +142,12 @@ const PermitAllowance = ({ contract }) => {
             const contract = new web3.eth.Contract(contractABI, contractAddress);
             const accounts = await web3.eth.getAccounts();
 
-            const tx = await contract.methods.setERC721Allowance(erc721TokenAddress, operator)
-                .send({ from: accounts[0] });
+            const tx = await contract.methods.setERC721Allowance(erc721TokenAddress, operator).send({ from: accounts[0] });
             setERC721Allow(tx);
             setToggleHash721(true);
         } catch (error) {
-            console.error('Error setting ERC721 allowance:', error);
-            setERC721Allow('setting ERC721 allowance', error);
+            console.error("Error setting ERC721 allowance:", error);
+            setERC721Allow("setting ERC721 allowance", error);
             setToggleHash721(false);
         }
     }
@@ -170,33 +170,61 @@ const PermitAllowance = ({ contract }) => {
         setShowForm721(!showForm721);
     };
     return (
-
         <div className="formulario1">
-            <button className="button" onClick={approveERC20Token}>ERC20 APPROVE</button>
-            {erc20Approve && (
-                <div>
-                    <AlertComponent
-                        toggle={toggleHashApprove}
-                        message={erc20Approve}
-                        isCopied={isCopied}
-                        setIsCopied={setIsCopied}
+            <button className="button" onClick={approveERC20Token}>
+                ERC20 APPROVE
+            </button>
+            <button onClick={() => setToggleApprove(!toggleApprove)} className="toggleButton">
+                {toggleApprove ? "Hide Params" : "Show Params"}
+            </button>
+            {toggleApprove && (
+                <div className="formulario_grid">
+                    <label htmlFor="spenderInput">Spender:</label>
+                    <textarea
+                        rows="1"
+                        type="text"
+                        className="formulario_grid_input"
+                        id="spenderInput"
+                        value={spender}
+                        onChange={(e) => setSpender(e.target.value)}
+                    />
+                    <label htmlFor="valueInput">Value:</label>
+                    <textarea
+                        rows="1"
+                        type="text"
+                        className="formulario_grid_input"
+                        id="valueInput"
+                        value={value1}
+                        onChange={(e) => setValue(e.target.value)}
+                    />
+                    <label htmlFor="ownerInput">Owner:</label>
+                    <textarea
+                        rows="1"
+                        type="text"
+                        className="formulario_grid_input"
+                        id="ownerInput"
+                        value={owner}
+                        onChange={(e) => setOwner(e.target.value)}
                     />
                 </div>
             )}
-                <button className="button" onClick={permitERC20Token}>ERC20 PERMIT</button>
-                {erc20Allow && (
-                    <div>
-                        <AlertComponent
-                            toggle={toggleHash}
-                            message={erc20Allow}
-                            isCopied={isCopied}
-                            setIsCopied={setIsCopied}
-                        />
-                    </div>
-                )}
+
+            {erc20Approve && (
+                <div>
+                    <AlertComponent toggle={toggleHashApprove} message={erc20Approve} isCopied={isCopied} setIsCopied={setIsCopied} />
+                </div>
+            )}
+            <button className="button" onClick={permitERC20Token}>
+                ERC20 PERMIT
+            </button>
+            {erc20Allow && (
+                <div>
+                    <AlertComponent toggle={toggleHash} message={erc20Allow} isCopied={isCopied} setIsCopied={setIsCopied} />
+                </div>
+            )}
 
             <button onClick={toggleFormDisplay} className="toggleButton">
-                {showForm ? 'Hide Params' : 'Show Params'}
+                {showForm ? "Hide Params" : "Show Params"}
             </button>
             {showForm ? (
                 <div className="formulario_grid">
@@ -255,47 +283,25 @@ const PermitAllowance = ({ contract }) => {
                         rows="3"
                     />
                     <label htmlFor="rInput">r :</label>
-                    <textarea
-                        rows="2"
-                        type="text"
-                        className="formulario_grid_input"
-                        id="rInput"
-                        value={r1}
-                        readOnly
-                    />
+                    <textarea rows="2" type="text" className="formulario_grid_input" id="rInput" value={r1} readOnly />
                     <label htmlFor="sInput">s :</label>
-                    <textarea
-                        rows="2"
-                        type="text"
-                        className="formulario_grid_input"
-                        id="sInput"
-                        value={s1}
-                        readOnly
-                    />
+                    <textarea rows="2" type="text" className="formulario_grid_input" id="sInput" value={s1} readOnly />
                     <label htmlFor="vInput">v :</label>
-                    <textarea
-                        rows="1"
-                        type="text"
-                        className="formulario_grid_input"
-                        id="vInput"
-                        value={v1}
-                        readOnly
-                    />
-
-                </div>) : ""}
-                <button className="button" onClick={checkERC20Allowance}>ERC20 ALLOWANCE</button>
-                {erc20Check && (
-                    <div>
-                        <AlertComponent
-                            toggle={toggleCheck}
-                            message={erc20Check}
-                            isCopied={isCopied}
-                            setIsCopied={setIsCopied}
-                        />
-                    </div>
-                )}
+                    <textarea rows="1" type="text" className="formulario_grid_input" id="vInput" value={v1} readOnly />
+                </div>
+            ) : (
+                ""
+            )}
+            <button className="button" onClick={checkERC20Allowance}>
+                ERC20 ALLOWANCE
+            </button>
+            {erc20Check && (
+                <div>
+                    <AlertComponent toggle={toggleCheck} message={erc20Check} isCopied={isCopied} setIsCopied={setIsCopied} />
+                </div>
+            )}
             <button onClick={toggleFormDisplayAllowance} className="toggleButton">
-                {showFormAllowance ? 'Hide  Params' : 'Show  Params'}
+                {showFormAllowance ? "Hide  Params" : "Show  Params"}
             </button>
             {showFormAllowance ? (
                 <div className="formulario_grid">
@@ -326,21 +332,21 @@ const PermitAllowance = ({ contract }) => {
                         value={spender}
                         onChange={(e) => setSpender(e.target.value)}
                     />
-                </div>) : ""}
+                </div>
+            ) : (
+                ""
+            )}
 
-                <button className="button" onClick={setERC721Allowance}>ERC721 PERMIT</button>
-                {erc721Allow && (
-                    <div>
-                        <AlertComponent
-                            toggle={toggleHash721}
-                            message={erc721Allow}
-                            isCopied={isCopied}
-                            setIsCopied={setIsCopied}
-                        />
-                    </div>
-                )}
+            <button className="button" onClick={setERC721Allowance}>
+                ERC721 PERMIT
+            </button>
+            {erc721Allow && (
+                <div>
+                    <AlertComponent toggle={toggleHash721} message={erc721Allow} isCopied={isCopied} setIsCopied={setIsCopied} />
+                </div>
+            )}
             <button onClick={toggleFormDisplay721} className="toggleButton">
-                {showForm721 ? 'Hide Params' : 'Show Params'}
+                {showForm721 ? "Hide Params" : "Show Params"}
             </button>
             {showForm721 ? (
                 <div className="formulario_grid">
@@ -371,7 +377,10 @@ const PermitAllowance = ({ contract }) => {
                         value={contractAddress}
                         onChange={(e) => setContractAddress(e.target.value)}
                     />
-                </div>) : ""}
+                </div>
+            ) : (
+                ""
+            )}
         </div>
     );
 };
